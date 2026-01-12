3f32329b79fc0730ae77b5d73fb5567637b027cf
Nathan Melis <nathanmelis@gmail.com>
2026-01-12 17:00:58 +0100

    Update pkg/semver/parse.go
    Co-authored-by: nogitting <50910960+nogitting@users.noreply.github.com>

--- pkg/semver/parse.go
+++ pkg/semver/parse.go
@@ -24,10 +24,7 @@ func ParseTolerant(version string) (blangsemver.Version, error) {
 	return blangsemver.ParseTolerant(version)
 }
 
-// ParseWithOptions parses a version string into a semver version struct.
-// It tolerates certain version specifications that do not strictly adhere to semver specs.
-// See the library documentation for more information.
-// Returns the parsed blang/semver/v4 Version.
+// ParseWithOptions parses a version string into a semver version struct, in the given parse mode.
 func ParseWithOptions(version string, parseMode string, semVerPerPrefix bool, prefix string) (blangsemver.Version, error) {
 	switch parseMode {
 	case semverparse.Strict:
